34feb146f01f97de5a627fdb76976236679d6068
saddamalmahali <saddamalmahali@yahoo.co.id>
2017-01-26 05:20:27 +0700

    Tambah Aplikasi

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -7,9 +7,105 @@
                     <h3 class="panel-title">Tentang Aplikasi</h3>
                 </div>
                 <div class="panel-body">
-                    Mikhail Yazid Bustomi (1106075)
-                    Ramdani Setiawan (1306108)
-                    Rian Fardian Rizqi (1306115)
+                    <h3>Tim Pembuatan Aplikasi</h3>
+                    <table class="table">
+                        <tr>
+                            <th>Nama</th>
+                            <td>:</td>
+                            <td>Mikhail Yazid Bustomi</td>
+                        </tr>
+                        <tr>
+                            <th>NIM</th>
+                            <td>:</td>
+                            <td>1106075</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>:</td>
+                            <td>1106075@sttgarut.ac.id</td>
+                        </tr>
+                        <tr>
+                            <th>No. HP</th>
+                            <td>:</td>
+                            <td>081321358177</td>
+                        </tr>
+                    </table>
+
+                    
+                    <table class="table">
+                        <tr>
+                            <th>Nama</th>
+                            <td>:</td>
+                            <td>Ramdani Setiawan</td>
+                        </tr>
+                        <tr>
+                            <th>NIM</th>
+                            <td>:</td>
+                            <td>1306108</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>:</td>
+                            <td>1306108@sttgarut.ac.id</td>
+                        </tr>
+                        <tr>
+                            <th>No. HP</th>
+                            <td>:</td>
+                            <td>085797911017</td>
+                        </tr>
+                    </table>
+
+                    
+                    <table class="table">
+                        <tr>
+                            <th>Nama</th>
+                            <td>:</td>
+                            <td>Rian Fardian Rizqi</td>
+                        </tr>
+                        <tr>
+                            <th>NIM</th>
+                            <td>:</td>
+                            <td>1306115</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>:</td>
+                            <td>1306115@sttgarut.ac.id</td>
+                        </tr>
+                        <tr>
+                            <th>No. HP</th>
+                            <td>:</td>
+                            <td>081312228132</td>
+                        </tr>
+                    </table>
+
+                    <h3>Dosen Pembimbing Akademik : </h3>
+
+                    <table class="table">
+                        <tr>
+                            <th>Nama</th>
+                            <td>:</td>
+                            <td>Asri Mulyani, M.Kom</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>:</td>
+                            <td>asrimulyani@sttgarut.ac.id</td>
+                        </tr>
+                    </table>
+                    <h3>Pembimbing Lapangan :</h3>
+                    <table class="table">
+                        <tr>
+                            <th>Nama</th>
+                            <td>:</td>
+                            <td>Yayat Sudrajat</td>
+                        </tr>
+                        <tr>
+                            <th>Email</th>
+                            <td>:</td>
+                            <td>-</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
